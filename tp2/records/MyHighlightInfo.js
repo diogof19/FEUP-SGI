700e884676e2d3787765b4d10eb618d63c83838a
André Flores <andrejesusferflores@gmail.com>
2022-11-23 14:36:34 +0000

    Refactor - create info classes

---
+++ tp2/records/MyHighlightInfo.js
@@ -0,0 +1,6 @@
+export class MyHighlightInfo {
+    constructor(highlightColor, highlightScale) {
+        this.color = highlightColor;
+        this.scale = highlightScale;
+    }
+}
---
+++ tp2/records/MyTextureInfo.js
@@ -0,0 +1,7 @@
+export class MyTextureInfo {
+    constructor(textureId, lengthS, lengthT) {
+        this.textureId = textureId;
+        this.lengthS = lengthS;
+        this.lengthT = lengthT;
+    }
+}
