1221b6dfba1ba33ab15d2d22fce1a360215eaa8a
André Flores <andrejesusferflores@gmail.com>
2022-12-15 17:38:30 +0000

    Fix game state after undo

--- tp3/model/records/MyMoveRecord.js
+++ tp3/model/records/MyMoveRecord.js
@@ -1,10 +1,12 @@
 export class MyMoveRecord {
-    constructor(row, col, newRow, newCol, capturedPiece, playerBefore) {
+    constructor(row, col, newRow, newCol, capturedPiece, playerNumberBefore, player0CapturedBefore, player1CapturedBefore) {
         this.row = row;
         this.col = col;
         this.newRow = newRow;
         this.newCol = newCol;
         this.capturedPiece = capturedPiece;
-        this.playerBefore = playerBefore;
+        this.playerNumberBefore = playerNumberBefore;
+        this.player0CapturedBefore = player0CapturedBefore;
+        this.player1CapturedBefore = player1CapturedBefore;
     }
 }
